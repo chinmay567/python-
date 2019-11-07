ab8287489c97a2d37458a4a297effd017b782be8
chinmay567 <49231104+chinmay567@users.noreply.github.com>
2019-11-07 11:09:39 +0530

    Add files via upload

---
+++ python function.py
@@ -0,0 +1,39 @@
+'''
+def fucn_name():
+    blah...
+    blah...
+fucn_name()
+'''
+"""
+def student (name,clas,branch):
+    print('name: ',name,'\nsemister: ',clas,'\nBranch:',branch)
+    
+    print('\n')
+
+student('chinmay',5,'eee')
+student('Raja','8th','EE')
+student('puja','8th','cEE')
+student('xyz','1st','mechanical')
+"""
+
+'''
+def add(var1,var2):
+    print('Add:')
+    print(var1+var2)
+    
+def sub(var1,var2):
+    print('Sub:')
+    print(var1-var2)
+    
+def mulx(var1,var2):
+    print('Multi:')
+    print(var1*var2)
+    
+a = int(input('v1: '))
+b= int(input('v2: '))
+
+add(a,b)
+sub(a,b)
+mulx(a,b)
+'''
+
